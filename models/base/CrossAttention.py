fa94ffbeb29cea25c88b13d5da3a9b1b60fcfcd2
Khoawawa <atom08072004@gmail.com>
2025-11-08 15:49:08 +0700

    a

--- models/base/CrossAttention.py
+++ models/base/CrossAttention.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 class LayerNormCA(torch.nn.Module):
     def __init__(self, dim_q, dim_kv, num_heads=8,batch_first=True):
         super().__init__()
@@ -8,8 +9,8 @@ class LayerNormCA(torch.nn.Module):
         self.ln = torch.nn.LayerNorm(dim_q)
     def forward(self, query, key_value, attn_mask=None,key_padding_mask=None):
         # implement cross attention mechanism
-        query = self.ln_q(query)
-        key_value = self.ln_kv(key_value)
+        query = F.normalize(self.ln_q(query), dim=-1)
+        key_value =  F.normalize(self.ln_kv(key_value), dim=-1)
         fused_feat = self.ca(query, key_value,attn_mask=attn_mask,key_padding_mask=key_padding_mask)
         if torch.isnan(fused_feat).any():
             print("NaN detected: Output of self.ca is NaN!")
