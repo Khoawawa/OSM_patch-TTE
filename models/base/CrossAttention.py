c1b5b5759bfa12cf25d8f65a8c50fff5fb7d6644
Khoawawa <atom08072004@gmail.com>
2025-11-09 01:29:33 +0700

    a

--- models/base/CrossAttention.py
+++ models/base/CrossAttention.py
@@ -1,5 +1,8 @@
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
+# from flash_attn_interface import flash_attn_func
+
 class LayerNormCA(torch.nn.Module):
     def __init__(self, dim_q, dim_kv, num_heads=8,batch_first=True):
         super().__init__()
@@ -21,5 +24,14 @@ class CrossAttention(torch.nn.Module):
     def forward(self, query, key_value, attn_mask=None,key_padding_mask=None):
         # implement cross attention mechanism
         with torch.autocast(device_type='cuda', enabled=False):
-            fused_feat, _ = self.attn(query, key_value, key_value, attn_mask=attn_mask,key_padding_mask=key_padding_mask)
+            fused_feat, _ = self.attn(query.float(), key_value.float(), key_value.float(), attn_mask=attn_mask,key_padding_mask=key_padding_mask)
         return fused_feat
+
+# class FlashMHA(torch.nn.Module):
+#     def __init__(self, dim_q, dim_kv, num_heads=8,batch_first=True):
+#         super().__init__()
+
+#         self.q_proj = nn.Linear(dim_q, dim_q)
+#         self.kv_proj = nn.Linear(dim_kv, dim_q)
+
+#         self.out_proj = nn.Linear(dim_q, dim_q)
