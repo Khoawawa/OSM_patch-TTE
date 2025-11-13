d1f41a12defef079839c224a747365be27cac4bf
Khoawawa <atom08072004@gmail.com>
2025-11-13 14:23:58 +0700

    a

--- models/OSM_BE_Resnet_TTE.py
+++ models/OSM_BE_Resnet_TTE.py
@@ -47,10 +47,10 @@ class OSM_BER_TTE(torch.nn.Module):
         representation = representation if batch_first else representation.transpose(0,1).contiguous() # (T,B,Res + Ctx)
         hiddens, _ = self.temporal_block(representation, seq_lens = input_['lens'].long())
         assert not torch.isnan(hiddens).any(), "hidden has nan"
-        decoder = self.decoder(hiddens, input_['lens'].long()) # (T,B,seq_hidden_dim)
-        if torch.isnan(decoder).sum() > 0:
-            with torch.amp.autocast(enabled=False):
-                decoder = self.decoder(hiddens, input_['lens'].long())
+        # decoder = self.decoder(hiddens, input_['lens'].long()) # (T,B,seq_hidden_dim)
+        # if torch.isnan(decoder).sum() > 0:
+        with torch.amp.autocast(enabled=False):
+            decoder = self.decoder(hiddens, input_['lens'].long())
         decoder = decoder if batch_first else decoder.transpose(0,1).contiguous() # (B,T,seq_hidden_dim)
             
         assert torch.isnan(decoder).sum() == 0, "decoder has nan"
