2fbde4b1cef8ae84d4e182f5629a74df228f4c53
Khoawawa <atom08072004@gmail.com>
2025-12-21 05:16:34 +0700

    a

--- train/train_model.py
+++ train/train_model.py
@@ -85,10 +85,10 @@ def train_model(model: nn.Module, data_loaders: Dict[str, DataLoader],
                         + desc
                     )
                     with torch.no_grad():
-                        pred_real = output.cpu().detach().numpy()
+                        pred_real = output.cpu().detach()
                         if args.log1p:
                             pred_real = torch.expm1(pred_real)
-                        predictions.append(pred_real)
+                        predictions.append(pred_real.numpy())
 
                     running_loss[phase] += loss.item() * truth_data.size(0)
                     # if step % 1000 == 0:
