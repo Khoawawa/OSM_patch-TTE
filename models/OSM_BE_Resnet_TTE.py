59dbca5a78abcca484b0e1f526ba14edfffeeabc
Khoawawa <atom08072004@gmail.com>
2025-12-21 04:52:45 +0700

    a

--- models/OSM_BE_Resnet_TTE.py
+++ models/OSM_BE_Resnet_TTE.py
@@ -17,12 +17,13 @@ class MulT_TTE(torch.nn.Module):
                  bert_attention_heads,bert_hidden_size,pad_token_id,bert_hidden_layers,vocab_size=27300):
         super().__init__()
         self.context_encoder = ContextEncoder(bert_attention_heads,bert_hidden_size,pad_token_id,bert_hidden_layers,vocab_size) # trip specific encoder
-        self.temporal_block = LayerNormGRU(input_dim=self.context_encoder.hidden_size, hidden_dim=seq_hidden_dim, num_layers=seq_layer)
         self.represent = nn.Sequential(
             nn.Linear(self.context_encoder.hidden_size, seq_hidden_dim),
             nn.LeakyReLU(),
             nn.Linear(seq_hidden_dim, seq_hidden_dim)
         )
+        self.temporal_block = LayerNormGRU(input_dim=seq_hidden_dim, hidden_dim=seq_hidden_dim, num_layers=seq_layer)
+        
         self.decoder = Decoder(d_model=seq_hidden_dim, N=decoder_layer)
         self.adanorm = AdaRMSNorm(d_model=seq_hidden_dim, d_context=33)
         self.pool_attn = nn.Linear(seq_hidden_dim, 1)
