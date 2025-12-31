36d80e54ad7ac6609a90f060590e65514ffb0c7b
Khoawawa <atom08072004@gmail.com>
2025-12-31 16:52:04 +0700

    a

--- models/OSM_BE_Resnet_TTE.py
+++ models/OSM_BE_Resnet_TTE.py
@@ -47,14 +47,15 @@ class MulT_TTE(torch.nn.Module):
         masked_h = h * mask
         max_pooled = masked_h.max(dim=1).values
         # weighted sum pooling
-        # masked_h = h * mask
-        # weights = seg_lens.float().unsqueeze(-1) # (B,T)
-        # masked_weights = weights * mask
-        # weighted_masked_h = masked_h * masked_weights
-        # sum_pooled = weighted_masked_h.sum(dim=1)
-        # total_weight = masked_weights.sum(dim=1).clamp(min=1e-6)
-        # sum_pooled = sum_pooled / total_weight
-        sum_pooled = self.sum_pooling(h, valid_mask)
+        masked_h = h * mask
+        
+        weights = seg_lens.float().unsqueeze(-1) # (B,T)
+        masked_weights = weights * mask
+        percentages = masked_weights / masked_weights.sum(dim=1, keepdim=True).clamp(min=1e-6)
+        
+        weighted_masked_h = masked_h * percentages
+        sum_pooled = weighted_masked_h.sum(dim=1)
+        # sum_pooled = self.sum_pooling(h, valid_mask)
         return torch.cat([max_pooled, sum_pooled], dim=-1)
     
     def forward(self, input_, args):
