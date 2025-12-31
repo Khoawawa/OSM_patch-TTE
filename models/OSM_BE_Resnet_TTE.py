2e5601ae12097b945567d74288bc811ca06906b3
Khoawawa <atom08072004@gmail.com>
2026-01-01 00:37:48 +0700

    a

--- models/OSM_BE_Resnet_TTE.py
+++ models/OSM_BE_Resnet_TTE.py
@@ -44,7 +44,7 @@ class MulT_TTE(torch.nn.Module):
     def max_sum_pooling(self, h : torch.Tensor, valid_mask: torch.Tensor, seg_lens):
         mask = valid_mask.float().unsqueeze(-1)
         # bottleneck identifier: max pooling
-        masked_h = h.masked_fill(mask == 0, float('-inf'))
+        masked_h = h.masked_fill(mask == 0, -1e9)
         max_pooled = masked_h.max(dim=1).values
         # weighted sum pooling
         
