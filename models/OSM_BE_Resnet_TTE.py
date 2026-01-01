9feef14704e0d2091213ed19e33f9a5c7adff438
Khoawawa <atom08072004@gmail.com>
2026-01-02 00:28:11 +0700

    a

--- models/OSM_BE_Resnet_TTE.py
+++ models/OSM_BE_Resnet_TTE.py
@@ -37,19 +37,22 @@ class MulT_TTE(torch.nn.Module):
     def forward(self, input_, args):
         # visual input
         valid_mask = input_['valid_mask']  # (B,T)
+
         representation, loss_1, (weekrep,daterep,timerep) = self.context_encoder(input_, args)
+
         representation = self.represent(representation) # (B,T,seq_hidden_dim)
         representation = representation if batch_first else representation.transpose(0,1).contiguous() # (T,B,Res + Ctx)
 
         hiddens, _ = self.temporal_block(representation, seq_lens = input_['lens'].long())
-        device_type = "cuda"
-        with torch.amp.autocast(device_type=device_type, enabled=False):
-            decoder = self.decoder(hiddens.float(), input_['lens'].long())
+
+        decoder = self.decoder(hiddens.float(), input_['lens'].long())
         decoder = decoder if batch_first else decoder.transpose(0,1).contiguous() # (B,T,seq_hidden_dim)
         # sum pooling
         decoder = decoder * valid_mask.unsqueeze(-1).float() # (B,T,seq_hidden_dim)
         pooled_decoder = decoder.sum(dim=1) # (B,seq_hidden_dim)
+
         pooled_decoder = torch.cat([pooled_decoder, weekrep[:,0], daterep[:,0], timerep[:,0]], dim=-1) # (B,seq_hidden_dim + 33)
+
         output = self.mlp(pooled_decoder) # (B,1)
         return output, loss_1
 
