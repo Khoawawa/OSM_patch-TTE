6b2f8414aa1cab6a1dedaedb824fcba87ec009ff
Khoawawa <atom08072004@gmail.com>
2026-01-26 16:36:16 +0700

    a

--- test/testing_main.py
+++ test/testing_main.py
@@ -56,6 +56,7 @@ def test_model(model, data_loader, args):
         f.write(f"{args.model_config}\n")
         f.write(f"{args.data_config}\n")
         f.write(f"{metric}\n\n")
+    np.save(os.path.join(args.model_folder, "result.npy"), np.asarray([pre2, inds]))
     json_log = {
         k: np.concatenate(v).astype(float).tolist()
         for k, v in log.items()
@@ -63,7 +64,7 @@ def test_model(model, data_loader, args):
     with open(f'{args.absPath}/data/log_{args.model}.json', 'w') as f:
         json.dump(json_log, f, indent=4)
         f.write('\n')
-    np.save(os.path.join(args.model_folder, "result.npy"), np.asarray([pre2, inds]))
+    
 
 
 def test_main(args):
