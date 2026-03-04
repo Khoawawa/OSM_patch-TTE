f341c8e2b5396cadf4e85c1f3f235a25cf2ebd3a
Khoawawa <atom08072004@gmail.com>
2026-03-04 17:39:44 +0700

    a

--- utils/prepare.py
+++ utils/prepare.py
@@ -193,8 +193,6 @@ def create_model(args):
     with open(absPath) as file:
         model_config = json.load(file)[args.model]
     args.model_config = model_config
-    model_config['pad_token_id'] = args.data_config['edges'] + 1
-    
     return POI_MulT_TTE(**model_config)
         
 
